3fa692760f13a0099097101653422e2f0220afc8
Dayne Freudenberg <dfr241@uky.edu>
2024-12-03 22:25:14 -0500

    my commit didnt work on vscode ;(

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# required modules 
 import random 
 import json 
 import pickle 
 import numpy as np 
 import nltk 
+import keras
 from keras.models import load_model 
 from nltk.stem import WordNetLemmatizer 
 
@@ -13,62 +13,59 @@ lemmatizer = WordNetLemmatizer()
 intents = json.loads(open("intense.json").read()) 
 words = pickle.load(open('words.pkl', 'rb')) 
 classes = pickle.load(open('classes.pkl', 'rb')) 
-model = load_model('chatbotmodel.h5') 
+
+# Change to .keras if that's your format
+model = load_model('billy-buster.keras') 
 
 def clean_up_sentences(sentence): 
-	sentence_words = nltk.word_tokenize(sentence) 
-	sentence_words = [lemmatizer.lemmatize(word) 
-					for word in sentence_words] 
-	return sentence_words 
+    sentence_words = nltk.word_tokenize(sentence) 
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words] 
+    return sentence_words 
 
 def bagw(sentence): 
-	
-	# separate out words from the input sentence 
-	sentence_words = clean_up_sentences(sentence) 
-	bag = [0]*len(words) 
-	for w in sentence_words: 
-		for i, word in enumerate(words): 
-
-			# check whether the word 
-			# is present in the input as well 
-			if word == w: 
-
-				# as the list of words 
-				# created earlier. 
-				bag[i] = 1
-
-	# return a numpy array 
-	return np.array(bag) 
+    # separate out words from the input sentence 
+    sentence_words = clean_up_sentences(sentence) 
+    bag = [0] * len(words) 
+    for w in sentence_words: 
+        for i, word in enumerate(words): 
+            # check whether the word is present in the input
+            if word == w: 
+                bag[i] = 1
+    # return a numpy array 
+    return np.array(bag) 
 
 def predict_class(sentence): 
-	bow = bagw(sentence) 
-	res = model.predict(np.array([bow]))[0] 
-	ERROR_THRESHOLD = 0.25
-	results = [[i, r] for i, r in enumerate(res) 
-			if r > ERROR_THRESHOLD] 
-	results.sort(key=lambda x: x[1], reverse=True) 
-	return_list = [] 
-	for r in results: 
-		return_list.append({'intent': classes[r[0]], 
-							'probability': str(r[1])}) 
-		return return_list 
+    bow = bagw(sentence) 
+    res = model.predict(np.array([bow]), verbose=0)[0]  # ensure input shape is correct
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD] 
+    results.sort(key=lambda x: x[1], reverse=True) 
+    return_list = [] 
+    for r in results: 
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])}) 
+    return return_list 
 
 def get_response(intents_list, intents_json): 
-	tag = intents_list[0]['intent'] 
-	list_of_intents = intents_json['intents'] 
-	result = "" 
-	for i in list_of_intents: 
-		if i['tag'] == tag: 
-			
-			# prints a random response 
-			result = random.choice(i['responses']) 
-			break
-	return result 
+    tag = intents_list[0]['intent'] 
+    list_of_intents = intents_json['intents'] 
+    result = "" 
+    for i in list_of_intents: 
+        if i['tag'] == tag: 
+            # prints a random response 
+            result = random.choice(i['responses']) 
+            break
+    return result 
 
 print("Chatbot is up!")
 
 while True: 
-	message = input("") 
-	ints = predict_class(message) 
-	res = get_response(ints, intents) 
-	print(res) 
+    message = input("> ") 
+    if message.lower() == "quit":
+        break
+    
+    ints = predict_class(message) 
+    if ints:
+        res = get_response(ints, intents) 
+        print(res)
+    else:
+        print("Sorry, I didn't understand that. Please try again!")
